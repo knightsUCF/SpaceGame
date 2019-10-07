86d6a5f1c4260959e7ccd763f77d43698a462594
KnightsUCF <peterk@knights.ucf.edu>
2019-10-07 17:45:51 -0400

    Update Settings.cs

--- code/settings/Settings.cs
+++ code/settings/Settings.cs
@@ -1,7 +1,11 @@
+using UnityEngine;
+
+
+
 
 public class Settings : Singleton<State>
 {
-  public static float SPAWN_WAIT;
-  public static float START_WAIT;
-  public static float WAVE_WAIT;
+    public static float SPAWN_WAIT;
+    public static float START_WAIT;
+    public static float WAVE_WAIT;
 }
