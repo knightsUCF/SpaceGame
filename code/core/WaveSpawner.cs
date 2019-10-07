6a8379b9a1152adf6b7752981d0db7aff4435ced
KnightsUCF <peterk@knights.ucf.edu>
2019-10-06 23:59:16 -0400

    Update WaveSpawner.cs

--- code/core/WaveSpawner.cs
+++ code/core/WaveSpawner.cs
@@ -1,5 +1,29 @@
 class WaveSpawner : MonoBehaviour
 {
 
+  IEnumerator SpawnWaves()
+      {
+          yield return new WaitForSeconds(Settings.START_WAIT);
+          while (true)
+          {
+              for (int i = 0; i < hazardCount; i++)
+              {
+                  GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                  Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                  Quaternion spawnRotation = Quaternion.identity;
+                  Instantiate(hazard, spawnPosition, spawnRotation);
+                  yield return new WaitForSeconds(Settings.SPAWN_WAIT);
+              }
+              yield return new WaitForSeconds(Settings.WAVE_WAIT);
+
+              if (State.GAME_OVER)
+              {
+                  restartText.text = "Press 'R' for Restart";
+                  restart = true;
+                  break;
+              }
+          }
+      }
+
 
 }
