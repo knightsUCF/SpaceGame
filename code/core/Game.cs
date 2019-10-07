6806c5aa7d211e0e84649896ce9a7cba8e15c3c2
KnightsUCF <peterk@knights.ucf.edu>
2019-10-06 23:52:59 -0400

    Update Game.cs

--- code/core/Game.cs
+++ code/core/Game.cs
@@ -5,32 +5,39 @@ using UnityEngine.UI;
 using TMPro;
 
 
+
+
+
+
+
 public class Game : MonoBehaviour
 {
+
+    // game settings
+
     public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
-    public float spawnWait;
-    public float startWait;
-    public float waveWait;
+    
 
 
+    // text
+    
     public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI levelText;
     public TextMeshProUGUI gameOverText;
 
 
-    private bool restart;
-    private int score;
 
-    public int currentLevel = 1;
+    private bool restart;
+
+
 
     void Start()
     {
         restart = false;
         restartText.text = "";
         gameOverText.text = "";
-        score = 0;
+
         UpdateScore();
         StartCoroutine(SpawnWaves());
 
@@ -57,7 +64,7 @@ public class Game : MonoBehaviour
 
     IEnumerator SpawnWaves()
     {
-        yield return new WaitForSeconds(startWait);
+        yield return new WaitForSeconds(Settings.START_WAIT);
         while (true)
         {
             for (int i = 0; i < hazardCount; i++)
@@ -66,9 +73,9 @@ public class Game : MonoBehaviour
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds(spawnWait);
+                yield return new WaitForSeconds(Settings.SPAWN_WAIT);
             }
-            yield return new WaitForSeconds(waveWait);
+            yield return new WaitForSeconds(Settings.WAVE_WAIT);
 
             if (State.GAME_OVER)
             {
@@ -81,13 +88,13 @@ public class Game : MonoBehaviour
 
     public void AddScore(int newScoreValue)
     {
-        score += newScoreValue;
+        State.SCORE += newScoreValue;
         UpdateScore();
     }
 
     void UpdateScore()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Score: " + State.SCORE;
     }
 
     public void GameOver()
@@ -97,106 +104,6 @@ public class Game : MonoBehaviour
     }
 
 
-    public void NewLevel(int level)
-    {
-        levelText.text = "LEVEL " + level.ToString();
-    }
 
-
-
-    void ClearLevelText()
-    {
-        levelText.text = "";
-    }
-
-
-    void CheckIfToAdvanceToNextLevel()
-    {
-        if (score > 300 && currentLevel == 1)
-        {
-            currentLevel = 2;
-            NewLevel(2);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-        if (score > 600 && currentLevel == 2)
-        {
-            currentLevel = 3;
-            NewLevel(3);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-        if (score > 900 && currentLevel == 3)
-        {
-            currentLevel = 4;
-            NewLevel(4);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-        if (score > 1200 && currentLevel == 4)
-        {
-            currentLevel = 5;
-            NewLevel(5);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-        if (score > 1500 && currentLevel == 5)
-        {
-            currentLevel = 6;
-            NewLevel(6);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-        if (score > 1800 && currentLevel == 6)
-        {
-            currentLevel = 7;
-            NewLevel(7);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-        if (score > 2100 && currentLevel == 7)
-        {
-            currentLevel = 8;
-            NewLevel(8);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-        if (score > 2400 && currentLevel == 8)
-        {
-            currentLevel = 9;
-            NewLevel(9);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-        if (score > 2700 && currentLevel == 9)
-        {
-            currentLevel = 10;
-            NewLevel(10);
-            Invoke("ClearLevelText", 2);
-            hazardCount = hazardCount * 2;
-        }
-
-
-    }
-
-
-
-
-    /*
     
-    HazardCount is the number of obstacles, so as we increase levels, let's increase this hazard count by a multiple
-
-
-    if the score reaches a threshold, and there are no hazards in the area then the flash the level 2
-    
-    
-    */
 }
