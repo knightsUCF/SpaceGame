4fa0894ec56f1c3a9e427660c9f3a82a519668cf
KnightsUCF <peterk@knights.ucf.edu>
2019-10-06 23:55:19 -0400

    Update InputManager.cs

--- code/core/InputManager.cs
+++ code/core/InputManager.cs
@@ -4,7 +4,7 @@ class InputManager()
 
   void Update()
       {
-          if (restart)
+          if (restart) // get reference to restart
           {
               if (Input.GetKeyDown(KeyCode.R) || Input.GetButton("Fire1"))
               {
