70bfd5a9f0e616bd9bb68db7bf867c64fd9a673e
KnightsUCF <peterk@knights.ucf.edu>
2019-10-07 19:02:22 -0400

    Update DestroyByBoundary.cs

--- code/space object/general/DestroyByBoundary.cs
+++ code/space object/general/DestroyByBoundary.cs
@@ -1,1 +1,12 @@
+using UnityEngine;
+using System.Collections;
 
+
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
