188713f8b751b021f7cf891ad342aa173feeae70
KnightsUCF <peterk@knights.ucf.edu>
2019-10-07 19:05:07 -0400

    Update DestroyByTime.cs

--- code/space object/general/DestroyByTime.cs
+++ code/space object/general/DestroyByTime.cs
@@ -1,1 +1,15 @@
+using UnityEngine;
+using System.Collections;
 
+
+
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float lifetime;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
