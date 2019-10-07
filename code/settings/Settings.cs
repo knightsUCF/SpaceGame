e36c0addd67dbcc134bc0e7e1e5dd600860448cd
KnightsUCF <peterk@knights.ucf.edu>
2019-10-07 18:04:49 -0400

    Update Settings.cs

--- code/settings/Settings.cs
+++ code/settings/Settings.cs
@@ -6,7 +6,32 @@
 
 public class Settings : Singleton<State>
 {
+
+    // WAVE SPAWNING //////////////////////////////
+
+
+    // start wait time before the game begins
+
+    public static float START_WAIT = 3.0f;
+
+
+    // time between wave spawns
+
     public static float SPAWN_WAIT = 2.0f;
-    public static float START_WAIT;
+
+
+
     public static float WAVE_WAIT;
+
+
+
+    // number of hazardous objects
+
+    public static int HAZARD_COUNT = 2;
+
+
+    ///////////////////////////////////////////////
+
+
+    
 }
