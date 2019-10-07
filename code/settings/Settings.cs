51f930633712372a705c6647903b61c6da52aea2
KnightsUCF <peterk@knights.ucf.edu>
2019-10-07 17:47:59 -0400

    Update Settings.cs

--- code/settings/Settings.cs
+++ code/settings/Settings.cs
@@ -1,11 +1,12 @@
-// using UnityEngine; do we need this?
+
+
 
 
 
 
 public class Settings : Singleton<State>
 {
-    public static float SPAWN_WAIT;
+    public static float SPAWN_WAIT = 2.0f;
     public static float START_WAIT;
     public static float WAVE_WAIT;
 }
