812ef0f91a4b4a7394760c0f755698d9adb9a137
KnightsUCF <peterk@knights.ucf.edu>
2019-10-06 23:57:34 -0400

    Update Score.cs

--- code/core/Score.cs
+++ code/core/Score.cs
@@ -3,6 +3,18 @@ class Score : MonoBehaviour
 {
 
   public TextMeshProUGUI scoreText;
+  
+  
+    public void AddScore(int newScoreValue)
+    {
+        State.SCORE += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + State.SCORE;
+    }
 
 
 }
