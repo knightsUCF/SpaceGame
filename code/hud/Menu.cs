5d02a83f9bf0cd6e040106c40348f6d17424283f
KnightsUCF <peterk@knights.ucf.edu>
2019-10-09 23:55:13 -0400

    Update Menu.cs

--- code/hud/Menu.cs
+++ code/hud/Menu.cs
@@ -1,1 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Menu : MonoBehaviour
+{
+  
+    public GameObject menu;
+    public GameObject proceduralPanel;
+    public GameObject audioSource;
+
+
+
+
+    Game game;
+
+
+
+    private void Start()
+    {
+        game = FindObjectOfType<Game>();
+    }
+
+
+
+
+    public void OnMenuClick()
+    {
+        Debug.Log("Clicked on the menu");
+        audioSource.GetComponent<AudioSource>().Play();
+        menu.SetActive(false);
+        proceduralPanel.SetActive(true);
+    }
+
+
+
+    public void OnGenerateGalaxyClick()
+    {
+        Debug.Log("Generating galaxy");
+        audioSource.GetComponent<AudioSource>().Play();
+        proceduralPanel.SetActive(false);
+        game.GameStart();
+    }
+    
+}
 
